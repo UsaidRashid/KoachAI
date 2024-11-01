1fb6fd56e7f59dc66db91c762d09b9a7f023c514
UsaidRashid <rashidusaid2@gmail.com>
2024-11-01 15:51:55 +0530

    Task 1 Error handling

--- Task-1/backend/controllers/users.js
+++ Task-1/backend/controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/users");
+const { ValidationError } = require("mongoose").Error;
 
 module.exports.fetchAllUsers = async (req, res) => {
   try {
@@ -17,15 +18,34 @@ module.exports.addUser = async (req, res) => {
   try {
     const { age, name } = req.body;
 
-    const newUser = new User({ name, age });
+    if (!name || typeof name !== "string") {
+      return res.status(400).json({
+        message: "Invalid input: 'name' is required and must be a string.",
+      });
+    }
+    if (age === undefined || typeof age !== "number") {
+      return res.status(400).json({
+        message: "Invalid input: 'age' is required and must be a number.",
+      });
+    }
 
+    const newUser = new User({ name, age });
     await newUser.save();
 
     return res
       .status(200)
       .json({ message: "User Saved Successfully!", newUser });
   } catch (error) {
-    console.error("Error: ", error);
-    return res.status(500).json({ message: "Internal Server Error", error });
+    if (error instanceof ValidationError) {
+      console.error("Validation Error: ", error);
+      return res
+        .status(400)
+        .json({ message: "Validation Error", error: error.message });
+    } else {
+      console.error("Error saving user: ", error);
+      return res
+        .status(500)
+        .json({ message: "Internal Server Error", error: error.message });
+    }
   }
 };
